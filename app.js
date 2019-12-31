87b6a8940ca5b99d79c69d7b8cbb9eb516e07e54
Osama Soliman <eng.microsmsm@gmail.com>
2020-01-01 00:58:19 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+let treesize = 10;
+let counter = treesize;
+let arr = [];
+//tree
+for (let i = 0; i < treesize; i++) {
+    arr.push(' '.repeat(counter) + '*'.repeat((i*2)+1));
+    counter--;
+
+}
+//trunk
+for (let i = 0; i < treesize/3; i++) {
+    arr.push(' '.repeat(treesize-1) + '*'.repeat((3)));
+}
+
+console.log(arr.join('\n'));
+
